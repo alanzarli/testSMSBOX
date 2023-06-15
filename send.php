060f597f7192e9b9fb0e9fc6d06a805cab97b929
alanzarli <105911064+alanzarli@users.noreply.github.com>
2023-06-15 16:34:09 +0200

    v4

--- send.php
+++ send.php
@@ -3,8 +3,7 @@
 
 $ch = curl_init();
 
-$data = array('file_id' => '12345', 'recipients' => '+33767700847');
-var_dump($data);
+$data = array('file_id' => '174283d5-6161-4ea3-8735-93afc1f90c94', 'recipients' => '+33767700847');
 
 $headers = [
     "Authorization: App pub-533278fa1c8055ca13d216348907a8cd-496efc19-8a6e-41d9-94a1-36b54fcc8748",
@@ -15,6 +14,8 @@ curl_setopt_array($ch, [
     CURLOPT_URL => "https://api.smsbox.net/vmm/1.0/xml/send?recipients=[MSISDNs]&file_id=[file_id]",
     CURLOPT_HTTPHEADER => $headers, 
     CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $data,
 ]);
 $finalData = curl_exec($ch);
         var_dump($finalData);
